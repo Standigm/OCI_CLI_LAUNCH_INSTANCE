c197335de27580d0a7908c72872f5b22a9461d7b
Minhyeong Lee <82868121+Min-duo@users.noreply.github.com>
2023-05-03 15:15:39 +0900

    Update main.py
    주석 보강

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import subprocess
 
+# 아래변수의 OCID 및 Key 는 example일 뿐이며 각 사용자의 환경에 맞도록 바꿔야함을 꼭 명심하기 바란다.
 tenancy_id = 'ocid1.tenancy.oc1..aaaaaaaau7pm7zmesj6g5ceelakgnxsbcflzizstpfau4jqm7g3grbn7nowa'
 compartment_id = 'ocid1.compartment.oc1..aaaaaaaapslmjx7oxwi6tievsu5z3ob7odkxivh6qbqt4y25l6xyvkrhos7q'
 public_subnet_id = 'ocid1.subnet.oc1.ap-seoul-1.aaaaaaaatfn6jhuwxkkd6rlsetpssnyoqo77bwqqbgx7lewbgjgdrvge77ea'
@@ -16,11 +17,14 @@ av_domain = 'MFEe:AP-SEOUL-1-AD-1'
 shape_test = 'VM.Standard2.2'
 shape = 'BM.GPU.A10.4'
 
-
 ssh_auhorized_keys = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDNmuprd9Lo/3EUncSFkxn4JQB7HsHNVcyik5A8rlURzQxXat09XjvOWU43cPjl69yvwVW1wx+8cQbyZZrLZ+oNG+wDkSpZa6ed4JUESl/fuGmwBpOiyz1/nwr0RUd1Isdh1Fj3IC+mGEau1BidK5XzUv5/Jo4Akn1EoOBUepgKR6sUXemayakCe7aWUUg/TxZJFRbYd5SrLtxfm1mo2r8ZBXF8A/HkO/FLb/rtNbn1WwAYDQBhjjSjzZ238h0I4WjCU3ENiLED0Sd7chzjh1SEuss3Vs3OTkM54m3/aCcT+t0Im4tk4mbg8DElaaKDzt120nHlMonoI0DwW5nY8KiciF+N7/xmKklLCUSmLBPWYD9pkNgr1riUichJt7E8BbbFO3FZJlHyVnw1IM4U3Ylc1oLZIXIFiyfvmCvcqebMxMOlNTsdV0GQTQmpLmTkFEkgvVbzODtiVwb32WEOUUAyAr46Hv1yfqytKlvlkxB3zWUORU5oMEHitToBjpMP4Hk= minhyeonglee@standigms-MacBook.local'
-hostname = 'fep-instance-a10-'
 user_data_file = 'cloud-init.txt'
+
+# hostname 및 instance 의 label은 hostname + host_count 로 구성된다.
+# 중복되는 instance는 provisioning 되지 않으므로 test시 또는 여러 머신을 provisioning 하는 경우 사용하기위한 counter
+# 필요에 따라 이 부분 또한 적절히 수정하여 사욜하면 된다.
 host_count = 1
+hostname = 'fep-instance-a10-'
 
 # 인스턴스 프로비저닝
 # 여기서 shape, shape_test 변수를 교체해서 사용하면 된다.
